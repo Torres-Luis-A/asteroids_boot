62004b65159b7741bf507364abd07836f5b5cb0d
Torres-Luis-A <torresla960@gmail.com>
2025-11-22 13:25:16 -0500

    Added player sprite and hit box

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     VERSION = pygame.version.ver
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = clock.tick(60) / 1000
     while True:
@@ -15,7 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
+
 if __name__ == "__main__":
     main()

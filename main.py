9374886befc52431defb4f777b572f77fb288a8b
Torres-Luis-A <torresla960@gmail.com>
2025-12-01 10:29:53 -0500

    Moved the player to updatable and drable groups for better code structure.

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
+
 def main():
     pygame.init()
     VERSION = pygame.version.ver
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     clock = pygame.time.Clock()
-    dt = clock.tick(60) / 1000
+    
+    
     while True:
         log_state()
-
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)  
+        updatable.update(dt)
         pygame.display.flip()
-        clock.tick(60)
-
 if __name__ == "__main__":
     main()
